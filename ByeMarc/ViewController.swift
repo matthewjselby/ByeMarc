8bfdd7052b85189e9e09cbcbdfdd586e3103c08d
Matthew Selby <matthewselby@matthews-macbook-pro.local>
2016-12-20 09:48:14 -0500

    Initial Commit

---
+++ ByeMarc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ByeMarc
+//
+//  Created by Matthew Selby on 12/20/16.
+//  Copyright © 2016 Matthew Selby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
